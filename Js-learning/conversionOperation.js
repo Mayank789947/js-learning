12ccd52f34bc8f547affd4d63d33cbf4ffd24196
Mayank789947 <71781487+Mayank789947@users.noreply.github.com>
2025-04-14 13:00:41 +0000

    learn about conversion operations

--- Js-learning/conversionOperation.js
+++ Js-learning/conversionOperation.js
@@ -25,6 +25,32 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let number = 44
 
 let numberInString = String(number)
-console.log(numberInString)
-console.log(typeof numberInString)
+// console.log(numberInString)
+// console.log(typeof numberInString)
+
+
+//***************************** Operations ************************************ */
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%3)
+
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + "2")
+// console.log("1" + "2" + 2)
+// console.log(1 + 2 + "2")
+
+
+// console.log(true)
+// console.log(+true)
+// console.log(+"")
 
